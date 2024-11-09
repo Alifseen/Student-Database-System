48bf0f17ad29cae72ca5f358d05a253b046eebc2
Alifseen <alifseen@gmail.com>
2024-11-09 16:31:51 +0500

    Load SQL Data in the Table

--- Section 46.py
+++ Section 46.py
@@ -1,6 +1,7 @@
-from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QGridLayout, QLineEdit, QPushButton, QComboBox, QMainWindow, QTableWidget
+from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QGridLayout, QLineEdit, QPushButton, QComboBox, QMainWindow, QTableWidget, QTableWidgetItem
 import sys
 from PyQt6.QtGui import QAction
+import sqlite3
 
 
 ## Add a Main Window
@@ -24,12 +25,31 @@ class MainWindow(QMainWindow):
         self.table.setColumnCount(4)  ## Sets columns to 4
         self.table.setHorizontalHeaderLabels(("ID", "NAME", "SUBJECT", "MOBILE"))
 
+        self.table.verticalHeader().setVisible(False)  ## Removes the row number display
+
         self.setCentralWidget(self.table)  ## Makes the table widget the main widget
+
+    ## Load SQL Data into the Table
+    def load_data(self):
+        ## Load DB and get data
+        connection = sqlite3.connect("Files/database.db")
+        cursor = connection.cursor()
+        data = cursor.execute("SELECT * FROM students")
+
+        self.table.setRowCount(0)  ## This makes sure tha data is loaded in the table from row 1 column 1, not whereever the last entry was from.
+
+        for row_no, row_data in enumerate(data):  ## loops over index and tuples from the SQL data
+            self.table.insertRow(row_no)  ## Adds a row
+            for col_no, cell_data in enumerate(row_data):  ## loops over the tuple
+                self.table.setItem(row_no, col_no, QTableWidgetItem(str(cell_data)))  ## Adds the text into the cell
+
+        connection.close()
 
 
 
 
 app = QApplication(sys.argv)
 sms = MainWindow()
+sms.load_data()  ## Loads SQL Data on the Table
 sms.show()
 sys.exit(app.exec())
